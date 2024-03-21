aa714d6ccce501d9cbd4d21ffdb2528bf9d130c2
realtee <tolanialebiosu@gmail.com>
2024-03-21 13:07:23 +0100

    first commit

---
+++ src/main/java/com/tolani/ems/mapper/EmployeeMapper.java
@@ -0,0 +1,25 @@
+package com.tolani.ems.mapper;
+
+import com.tolani.ems.dto.EmployeeDto;
+import com.tolani.ems.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFistName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
---
+++ src/main/java/com/tolani/ems/service/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.tolani.ems.service;
+
+import com.tolani.ems.dto.EmployeeDto;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployee();
+
+    EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee);
+
+    void deleteEmployee(Long employeeId);
+
+    }
+
